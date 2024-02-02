2551a858f66ad595e8e48282f587015348b045a0
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2024-02-02 11:19:19 +0100

    chore: prepare Python 3.11 compatibility

--- train_mmlab_kie_process.py
+++ train_mmlab_kie_process.py
@@ -374,7 +374,8 @@ class TrainMmlabKieFactory(dataprocess.CTaskFactory):
         self.info.short_description = "Train for MMOCR from MMLAB KIE models"
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Text"
-        self.info.version = "2.0.0"
+        self.info.version = "2.0.1"
+        self.info.max_python_version = "3.10.0"
         self.info.icon_path = "icons/mmlab.png"
         self.info.authors = "Kuang, Zhanghui and Sun, Hongbin and Li, Zhizhong and Yue, Xiaoyu and Lin," \
                             " Tsui Hin and Chen, Jianyong and Wei, Huaqiang and Zhu, Yiqin and Gao, Tong and Zhang," \
